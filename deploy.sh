7d78b6a74d9f816cea1b0a67af64fa82f128a71b
Centell <centell@me.com>
2019-09-14 17:52:46 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp -R dist/ ../centell.github.io/
-cd ~/Documents/Workspace/Centell/Web/github.io/centell.github.io
+cp -R dist/ ../publish/
+cd ../publish
 git add .
 git commit -m 'auto deploy'
 git push
