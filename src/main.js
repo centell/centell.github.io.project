df1d213a406a0f17f49edb988d13e1960c0ce130
Centell <centell@me.com>
2019-09-14 17:58:22 +0900

    install semantic-ui-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import VueHeadful from 'vue-headful';
+import VueSemanticUI from 'semantic-ui-vue';
 
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(VueSemanticUI);
 
 Vue.config.productionTip = false;
 
