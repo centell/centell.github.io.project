af0ccbf3be03320545ffc219a725b2bfa30eab71
Centell <centell@me.com>
2019-09-14 16:42:31 +0900

    add VueHeadful for metatag

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <vue-headful v-bind="meta"/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,22 +9,31 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<!--<template>-->
+<!--  <div id="app">-->
+<!--    <vue-headful v-bind="meta" />-->
+
+<!--    <component v-bind:is="layout">-->
+<!--      <router-view />-->
+<!--    </component>-->
+
+<!--    <portal-target name="semantic-ui-vue" />-->
+<!--  </div>-->
+<!--</template>-->
+
+<script>
+export default
+{
+  data() {
+    return {
+      meta: {
+        title: "Centell's resume",
+        url: '/',
+      },
+    };
+  },
+};
+</script>
+<style scoped>
+  @import url('./assets/css/app.css');
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import VueHeadful from 'vue-headful';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.component('vue-headful', VueHeadful);
 
 new Vue({
   router,
